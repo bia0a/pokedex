a0473d0fa184d0ff14e4a7ce4ea6c8f0b5c57d0a
bia0a <anasilramos123@gmail.com>
2022-09-26 17:47:46 -0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonService } from './services/pokemon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonCardComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
